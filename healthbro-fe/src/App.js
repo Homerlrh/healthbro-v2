593d2f3b8571c39f54c5dd9cd2d1f93f2dbc810c
homer li <Homer.lee.0508@gmail.com>
2021-09-20 16:41:51 -0700

    add screen folder

--- healthbro-fe/src/App.js
+++ healthbro-fe/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 function App() {
+  // const click = async () => {
+  //   const body = {
+  //     email: "123@123.com",
+  //     password: "123321",
+  //   };
+  //   //const result = await axios.get(`http://localhost:3333`);
+  //   const result = await axios.post("http://localhost:3333/auth/login", body);
+  //   console.log(result);
+  // };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
