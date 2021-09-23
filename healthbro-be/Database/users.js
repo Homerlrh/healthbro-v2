6788d716362d63a65f1b8e9485d82456ad6bf2e4
homer li <Homer.lee.0508@gmail.com>
2021-09-23 05:01:04 -0700

    added unique constraint to user schema

--- healthbro-be/Database/users.js
+++ healthbro-be/Database/users.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // user document representation
 const userSchema = new mongoose.Schema({
   name: String,
-  email: String,
+  email: { type: String, unique: true },
   password: String,
   favourite: [String],
 });
