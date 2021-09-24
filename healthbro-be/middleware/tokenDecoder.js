cab93a9c7f662d6984dce79f739c3b3a8f370774
homer li <Homer.lee.0508@gmail.com>
2021-09-23 18:55:05 -0700

    change token extract method

--- healthbro-be/middleware/tokenDecoder.js
+++ healthbro-be/middleware/tokenDecoder.js
@@ -3,10 +3,10 @@ const jwt = require("jsonwebtoken");
 //extract data from jwt token and pass to another route to use
 const tokenDecoder = (req, res, next) => {
   //extract token value from the request body
-  const { token } = req.body;
+  const { authorization } = req.headers;
 
   //decode the token into object then assign it to the response obj
-  const decoded = jwt.decode(token);
+  const decoded = jwt.decode(authorization);
   res.user = decoded;
   next();
 };
