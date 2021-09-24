1cb850cb38a79de2803ce04454db0236d243f466
homer li <Homer.lee.0508@gmail.com>
2021-09-23 19:06:55 -0700

    added comment and error handling when requesting a endpoint thats not exist

--- healthbro-be/app.js
+++ healthbro-be/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(logger("dev"));
+app.use(logger("combined"));
 
 //compress all responses
 //speed up the responses
@@ -26,7 +26,20 @@ app.get("/", (req, res, next) => {
 app.use("/auth", require("./route/authRoute"));
 app.use("/api", require("./route/apiRoute"));
 
-//set up port, server run
+//response 404 error if the route requesting is not exist
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+//return a json object with error message
+app.use(function (err, req, res, next) {
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
+
+//set up port for server to run
 const port = process.env.PORT || "3333";
 const server = http.createServer(app);
 server.listen(port, () => {
